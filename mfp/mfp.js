7212484feace1442e3f1aed6abbed2a096ded199
Code Indie <11452791+codeindie@users.noreply.github.com>
2021-01-18 19:52:49 +0700

    Update mfp.js

--- mfp/mfp.js
+++ mfp/mfp.js
@@ -660,7 +660,7 @@
 });
 var arrLSN = ['smarthafiztalkingdoll.blogspot.com', 'www.smarthafiztalkingdoll.com', 'smarthafiztalkingdoll.com', 'unnicraft.blogspot.com', 'www.unnicraft.com', 'unnicraft.com',
              'gaspiknik.blogspot.com', 'www.gaspiknik.com', 'gaspiknik.com', 'largurda.blogspot.com', 'shop.largurda.id', 'www.shop.largurda.id', 'largurda.id', 'www.largurda.id',
-             'smarthafizpedia.blogspot.com', 'www.smarthafizpedia.com', 'smarthafizpedia.com', 'sativanacomputer.blogspot.com', 'www.sativana.net', 'sativana.net'];
+             'smarthafizpedia.blogspot.com', 'www.smarthafizpedia.com', 'smarthafizpedia.com', 'sativanacomputer.blogspot.com', 'www.sativana.net', 'sativana.net', 'tokorakbukuanak.blogspot.com'];
 var rdrUrl = 'https://bit.ly/shtd-404';
 var _0x29bf = ['location', '.ly/shtd-404', 'https://bit', 'length', 'href', 'undefined'];
 (function(_0x2553b2, _0x51f193) {
